9eca41c0f90a87239aad3652de66c442818e1e25
Triston Burns <tburns@aurasolutionsllc.com>
2026-05-16 14:39:42 -0700

    Add documentation for shouldSkipCommit function

--- src/infrastructure/git/skip-commit.ts
+++ src/infrastructure/git/skip-commit.ts
@@ -1,5 +1,11 @@
 import { CommitMetadataModel } from "../../domain/models/commit-metadata.model";
 
+/**
+ * Determines whether a commit should be skipped based on a skip token.
+ * @param {CommitMetadataModel} commit The commit metadata.
+ * @param {string} skipToken The token to look for in the commit message to determine if it should be skipped.
+ * @returns {boolean} True if the commit should be skipped, false otherwise.
+ */
 export const shouldSkipCommit = (
   commit: CommitMetadataModel,
   skipToken: string = "poet-skip") : boolean => {
